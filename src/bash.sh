2b0733fbe992d952ae3060be8a107e46852acd1a
HoseinRezaei <rezaeen77@gmail.com>
2023-11-03 21:02:01 +0330

    write Subtration and Multiplication

--- src/bash.sh
+++ src/bash.sh
@@ -4,4 +4,12 @@ x=10
 y=5
 echo "x=10, y=5"
 echo "Addition of x & y "
-echo $(( x+y))
+echo $((x+y))
+echo "Substration of x & y"
+echo $((x-y))
+echo "Multiplication of x & y"
+echo $((x*y))
+echo "Division of x by y"
+echo $((x/y))
+echo "Exponentiation of x,y"
+echo $((x ** y))
